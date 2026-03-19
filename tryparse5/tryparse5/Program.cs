73f35b9043aceb99d17f0ffad7b6a2c065055c0d
Axl Gómez <axljavigomez@gmail.com>
2026-03-19 17:32:08 -0600

    Verificar que el usuario ingrese un número entero

--- tryparse5/tryparse5/Program.cs
+++ tryparse5/tryparse5/Program.cs
@@ -4,4 +4,13 @@ do
 {
     Console.WriteLine("Ingrese un número");
     continuar = int.TryParse(Console.ReadLine(), out numero);
+    Console.Clear();
+    if (continuar)
+    {
+
+    }
+    else
+    {
+        Console.WriteLine("Dato inválido");
+    }
 } while (!continuar);
