5b45321eb85ebdedf6c93aec861155916bd0e000
Axl Gómez <axljavigomez@gmail.com>
2026-03-19 17:20:13 -0600

    Solicitar al usuario su fecha de nacimiento

---
+++ tryparse4/tryparse4/Program.cs
@@ -0,0 +1,7 @@
+﻿bool continuar;
+DateTime fecha;
+do
+{
+    Console.WriteLine("Ingrese su fecha de nacimiento");
+    continuar = DateTime.TryParse(Console.ReadLine(), out fecha);
+} while (!continuar);
