e1ee62ce6ba4a235cf9b32176e09cfd52a4f46b2
Axl Gómez <axljavigomez@gmail.com>
2026-03-19 17:04:38 -0600

    Solicitar al usuario la cantidad de estudiantes en el curso

---
+++ tryparse3/tryparse3/Program.cs
@@ -0,0 +1,7 @@
+﻿int estudiantes;
+bool continuar;
+do
+{
+    Console.WriteLine("Cuántos estudiantes hay en el curso?");
+    continuar = int.TryParse(Console.ReadLine(), out estudiantes);
+} while (!continuar);
