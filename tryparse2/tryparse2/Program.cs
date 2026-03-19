b49edf2efac7f32d64e966db07d18f2beec7dad4
Axl Gómez <axljavigomez@gmail.com>
2026-03-19 17:00:28 -0600

    Verificar que el precio ingresado sea un entero o un decimal, y que sea mayor a 0

--- tryparse2/tryparse2/Program.cs
+++ tryparse2/tryparse2/Program.cs
@@ -3,4 +3,15 @@ bool continuar;
 do
 {
     Console.WriteLine("Ingrese el precio de un producto");
+    continuar = double.TryParse(Console.ReadLine(), out precio);
+    Console.Clear();
+    if(continuar && precio > 0)
+    {
+        Console.WriteLine("El precio ingresado es válido");
+    }
+    else
+    {
+        Console.WriteLine("El precio ingresado no es válido");
+        continuar = false;
+    }
 } while (!continuar);
