4975600b6e9f10d0bed56e5614788322b01cde58
Axl Gómez <axljavigomez@gmail.com>
2026-03-19 17:46:48 -0600

    Verificar que el formato ingresado sea válido y mostrar la fecha

--- tryparse6/tryparse6/Program.cs
+++ tryparse6/tryparse6/Program.cs
@@ -4,4 +4,13 @@ do
 {
     Console.WriteLine("Ingrese una fecha");
     continuar = DateTime.TryParse(Console.ReadLine(), out fecha);
+    Console.Clear();
+    if (continuar)
+    {
+        Console.WriteLine($"La fecha ingresada es: {fecha.Day} {fecha.Month} {fecha.Year}");
+    }
+    else
+    {
+        Console.WriteLine("Dato inválido");
+    }
 } while (!continuar);
