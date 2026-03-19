2e9529e42067d44bb0d8b0383f334bf043cd6123
Axl Gómez <axljavigomez@gmail.com>
2026-03-19 16:50:21 -0600

    Verificar que la edad ingresada sea un número entero y esté dentro de un rango

--- tryparse1/tryparse1/Program.cs
+++ tryparse1/tryparse1/Program.cs
@@ -3,4 +3,15 @@ bool continuar;
 do
 {
     Console.WriteLine("Ingrese su edad:");
+    continuar = int.TryParse(Console.ReadLine(), out edad );
+    Console.Clear();
+    if(continuar && (edad>=0 && edad <= 120))
+    {
+        Console.WriteLine($"La edad {edad} es válida");
+    }
+    else
+    {
+        Console.WriteLine("Edad inválida");
+        continuar = false;
+    }
 } while (!continuar);
