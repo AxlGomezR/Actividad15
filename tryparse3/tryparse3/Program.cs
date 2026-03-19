0de49bb075a87693bcb9a606b25b74f614450ff8
Axl Gómez <axljavigomez@gmail.com>
2026-03-19 17:06:35 -0600

    Verificar que la cantidad ingresada sea un número entero positivo

--- tryparse3/tryparse3/Program.cs
+++ tryparse3/tryparse3/Program.cs
@@ -4,4 +4,14 @@ do
 {
     Console.WriteLine("Cuántos estudiantes hay en el curso?");
     continuar = int.TryParse(Console.ReadLine(), out estudiantes);
+    Console.Clear();
+    if(continuar && (estudiantes > 0))
+    {
+        Console.WriteLine("La cantidad ingresada es válida");
+    }
+    else
+    {
+        Console.WriteLine("La cantidad ingresada no es válida");
+        continuar = false;
+    }
 } while (!continuar);
