052732b1167fde837eb3c767f60b922eef1a0c7c
Axl Gómez <axljavigomez@gmail.com>
2026-03-19 17:24:23 -0600

    Verificar que la fecha de nacimiento sea coherente

--- tryparse4/tryparse4/Program.cs
+++ tryparse4/tryparse4/Program.cs
@@ -4,4 +4,13 @@ do
 {
     Console.WriteLine("Ingrese su fecha de nacimiento");
     continuar = DateTime.TryParse(Console.ReadLine(), out fecha);
+    Console.Clear();
+    if(continuar && (fecha.Year >= 1900 && fecha.Year <= 2026)){
+        Console.WriteLine("La fecha de nacimiento es válida");
+    }
+    else
+    {
+        Console.WriteLine("La fecha de nacimiento no es válida");
+        continuar = false;
+    }
 } while (!continuar);
