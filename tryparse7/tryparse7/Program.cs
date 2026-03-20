c5caf9a1a66ca827c0e796e7bfe148a00916b331
Axl Gómez <axljavigomez@gmail.com>
2026-03-19 18:01:50 -0600

    Solicitar al usuario el salario de un empleado

---
+++ tryparse7/tryparse7/Program.cs
@@ -0,0 +1,7 @@
+﻿bool continuar;
+double salario;
+do
+{
+    Console.WriteLine("Ingrese el salario de un empleado");
+    continuar = double.TryParse(Console.ReadLine(), out salario);
+} while (!continuar);
