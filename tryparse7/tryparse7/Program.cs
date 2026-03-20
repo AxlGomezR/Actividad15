f1b85312fc1de1f20919cbd2761ae6eacb4162ad
Axl Gómez <axljavigomez@gmail.com>
2026-03-19 18:03:46 -0600

    Verificar que el salario ingresado sea mayor a 0

--- tryparse7/tryparse7/Program.cs
+++ tryparse7/tryparse7/Program.cs
@@ -4,4 +4,14 @@ do
 {
     Console.WriteLine("Ingrese el salario de un empleado");
     continuar = double.TryParse(Console.ReadLine(), out salario);
+    Console.Clear();
+    if(continuar && salario > 0)
+    {
+        Console.WriteLine("El salario ingresado es válido");
+    }
+    else
+    {
+        Console.WriteLine("El salario ingresado no es válido");
+        continuar = false;
+    }
 } while (!continuar);
