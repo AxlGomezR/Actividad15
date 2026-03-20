f130f40e119899881f574dd1c997f030bfd9c41c
Axl Gómez <axljavigomez@gmail.com>
2026-03-19 18:11:27 -0600

    Verificar que el número sea mayor o igual a 0 y calcular la raiz

--- tryparse8/tryparse8/Program.cs
+++ tryparse8/tryparse8/Program.cs
@@ -4,4 +4,14 @@ do
 {
     Console.WriteLine("Ingrese un número");
     continuar = double.TryParse(Console.ReadLine(), out raiz);
+    Console.Clear();
+    if(continuar && raiz >= 0)
+    {
+        Console.WriteLine($"La raiz de {raiz} es = {Math.Sqrt(raiz)}");
+    }
+    else
+    {
+        Console.WriteLine("El número ingresado no es válido");
+        continuar = false;
+    }
 } while (!continuar);
