4de217303c11eace629a79886b0eb1f17a872d70
Axl Gómez <axljavigomez@gmail.com>
2026-03-19 18:26:44 -0600

    Solicitar al usuario la cantidad de productos que desea comprar

---
+++ tryparse10/tryparse10/Program.cs
@@ -0,0 +1,7 @@
+﻿bool continuar;
+int cantidad;
+do
+{
+    Console.WriteLine("Ingrese la cantidad de productos que desea comprar");
+    continuar = int.TryParse(Console.ReadLine(), out cantidad);
+} while (!continuar);
