08ab30582b49b249c1b61843e5bcec99b135434c
Axl Gómez <axljavigomez@gmail.com>
2026-03-19 18:15:26 -0600

    Solicitar al usuario la nota de un estudiante

---
+++ tryparse9/tryparse9/Program.cs
@@ -0,0 +1,7 @@
+﻿bool continuar;
+double nota;
+do
+{
+    Console.WriteLine("Ingrese la nota del estudiante");
+    continuar = double.TryParse(Console.ReadLine(), out nota);
+} while (!continuar);
