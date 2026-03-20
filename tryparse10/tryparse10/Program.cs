00b8fe1172acf5ac91056407b1ec6c4add7e7630
Axl Gómez <axljavigomez@gmail.com>
2026-03-19 18:33:09 -0600

    Simulación de ingreso de productos

--- tryparse10/tryparse10/Program.cs
+++ tryparse10/tryparse10/Program.cs
@@ -7,10 +7,21 @@ do
     Console.Clear();
     if(continuar && cantidad > 0)
     {
-
+        double total=0;
+        for(int i =0; i<cantidad; i++)
+        {
+            Console.WriteLine($"Producto {i+1}");
+            Console.WriteLine("Nombre del producto: ");
+            Console.ReadLine();
+            Console.WriteLine("Precio del producto");
+            total += double.Parse(Console.ReadLine());
+            Console.Clear();
+        }
+        Console.WriteLine($"El total a pagar es = {total}");
     }
     else
     {
         Console.WriteLine("La cantidad ingresada no es válida");
+        continuar = false;
     }
 } while (!continuar);
