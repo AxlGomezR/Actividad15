f317e7d52fd3072574e7fe5f3145e6b5348875e7
Axl Gómez <axljavigomez@gmail.com>
2026-03-19 18:28:52 -0600

    Verificar que el usuario ingrese un número entero mayor a 0

--- tryparse10/tryparse10/Program.cs
+++ tryparse10/tryparse10/Program.cs
@@ -4,4 +4,13 @@ do
 {
     Console.WriteLine("Ingrese la cantidad de productos que desea comprar");
     continuar = int.TryParse(Console.ReadLine(), out cantidad);
+    Console.Clear();
+    if(continuar && cantidad > 0)
+    {
+
+    }
+    else
+    {
+        Console.WriteLine("La cantidad ingresada no es válida");
+    }
 } while (!continuar);
