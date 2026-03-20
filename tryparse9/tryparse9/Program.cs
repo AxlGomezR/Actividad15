6f706a36c730b2bc4a5d79e57ec367bbab46586f
Axl Gómez <axljavigomez@gmail.com>
2026-03-19 18:17:27 -0600

    Verificar que la nota ingresada se encuentre dentro del rango

--- tryparse9/tryparse9/Program.cs
+++ tryparse9/tryparse9/Program.cs
@@ -4,4 +4,14 @@ do
 {
     Console.WriteLine("Ingrese la nota del estudiante");
     continuar = double.TryParse(Console.ReadLine(), out nota);
+    Console.Clear();
+    if(continuar && (nota>=0 && nota <= 100))
+    {
+
+    }
+    else
+    {
+        Console.WriteLine("La nota ingresada no es válida");
+        continuar = false;
+    }
 } while (!continuar);
